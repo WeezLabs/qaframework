3f013e1afbb7f0bd2e10a9da24b1366bdc0c031c
yurivan <yuri.ivanov@weezlabs.com>
2016-09-08 09:12:06 +0300

    Improvement for the DateTimeUtils fix.

--- src/main/java/util/DateTimeUtils.java
+++ src/main/java/util/DateTimeUtils.java
@@ -9,12 +9,12 @@ public class DateTimeUtils {
     }
 
     /**
-     * Compare two dates/times using accuracy accuracy.
+     * Compare two dates/times using accuracy.
      *
      * @param lhs first date/time to compare.
      * @param rhs second date/time to compare.
      * @param chronoUnit a unit of measurement that is used to compare date/time, {@link ChronoUnit#SECONDS} for example.
-     * @param accuracy maximum permissible difference in between two objects.
+     * @param accuracy maximum permissible difference between two objects.
      * @return True if difference between two dates/times no more than accuracy. False otherwise.
      */
     public static boolean compareWithAccuracy(Temporal lhs, Temporal rhs, ChronoUnit chronoUnit, long accuracy) {
